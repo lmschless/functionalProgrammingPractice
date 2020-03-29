062beb1a5f17cd02c3f05094a03f308e6929c6ad
Luke <lmschless@gmail.com>
2020-03-29 10:48:44 -0700

    adding files to project

---
+++ src/js-code.js
@@ -0,0 +1,9 @@
+// require('./js-code.js');
+
+console.log('Node'.concat(' is server-side.'));
+
+let dog = 'dog';
+
+let apple = 'apple';
+
+let test = 'test';
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './src/nyt-apis.js', // './src/index': './src/main.js'
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devtool: 'eval-source-map',
+	devServer: {
+		contentBase: './dist'
+	},
+	plugins: [
+		new UglifyJsPlugin({ sourceMap: true }),
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'NYT API Project',
+			template: './src/index.html',
+			inject: 'head'
+		}),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_module/,
+				loader: 'eslint-loader'
+			}
+		]
+	}
+};
