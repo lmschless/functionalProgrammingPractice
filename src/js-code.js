39912ae36631216f3bf0dbad9738d477eb7a8101
Luke <lmschless@gmail.com>
2020-03-29 13:40:20 -0700

    clean up

--- src/js-code.js
+++ src/js-code.js
@@ -1,10 +1,5 @@
 require('./js-code.js');
 
 // new linter can see that let is not allowed
-console.log('Node'.concat(' is server-side.'));
 
-let dog = 'dog';
-
-let apple = 'apple';
-
-let test = 'test';
+const coinCounter = () => {};
