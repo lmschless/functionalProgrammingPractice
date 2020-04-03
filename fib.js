686209bad928c6054a904b5e93e45749de9f5bb7
Luke <lmschless@gmail.com>
2020-04-02 20:59:48 -0700

    final commit tonight

--- fib.js
+++ fib.js
@@ -3,6 +3,7 @@ require('./fib.js');
 const findSum = (sum = 0, n1 = 1, n2 = 2) => {
 	() => {
 		if ((sum = 100)) {
+			console.log(n1, n2, sum);
 			return;
 		}
 		return findSum((sum = n1 + n2));
